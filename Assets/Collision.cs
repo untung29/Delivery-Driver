f5cb9c370e79fa2a3926f613606815e6a44d4f63
Untung <u.untung0029@gmail.com>
2022-01-02 15:43:47 +0700

    Trigger when the object get passed

--- Assets/Collision.cs
+++ Assets/Collision.cs
@@ -7,4 +7,8 @@ public class Collision : MonoBehaviour
     void OnCollisionEnter2D(Collision2D other) {
         Debug.Log("You bumped me!");
     }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        Debug.Log("You passed me!");    
+    }
 }
